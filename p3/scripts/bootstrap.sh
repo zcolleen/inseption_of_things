017fbb7989c9de424a4e16f63c1a72d77641513d
Shimat <philip.yevseev@gmail.com>
2021-11-19 00:25:44 +0300

    ubuntu-k3d-argocd

---
+++ p3/scripts/bootstrap.sh
@@ -0,0 +1,17 @@
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh && rm get-docker.sh
+
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+sudo curl -fsSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo chmod +x /usr/local/bin/argocd
+
+sudo k3d cluster create $CLUSTER
+export KUBECONFIG=$(sudo k3d kubeconfig write $CLUSTER)
+echo "export KUBECONFIG=$KUBECONFIG" >> $HOME/.bashrc
+sudo chown $(id -u):$(id -g) $KUBECONFIG
+
