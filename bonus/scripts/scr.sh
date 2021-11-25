97210f4fc3f42ef44a13b9c1a0b94db5733e2194
Shimat <philip.yevseev@gmail.com>
2021-11-25 23:07:13 +0300

    s

--- bonus/scripts/scr.sh
+++ bonus/scripts/scr.sh
@@ -9,7 +9,6 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
-chmod 0666 /var/run/docker.sock
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 k3d cluster create gitlab
 export KUBECONFIG=$(sudo k3d kubeconfig write gitlab)
