8148f96b3780812307a30d8ae666ffab49986e8d
Shimat <philip.yevseev@gmail.com>
2021-11-25 20:18:53 +0300

    svcaccounts ИЗМЕНЕН

---
+++ bonus/scripts/scr.sh
@@ -0,0 +1,21 @@
+       
+nslookup get.docker.com
+curl -fsSL https://get.docker.com | bash
+usermod -aG docker $USER
+
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
+chmod 0666 /var/run/docker.sock
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+k3d cluster create gitlab
+export KUBECONFIG=$(sudo k3d kubeconfig write gitlab)
+chmod 644 $KUBECONFIG
+kubectl create namespace gitlab
+kubectl create namespace dev
+helm repo add gitlab https://charts.gitlab.io
+helm install --namespace gitlab gitlab-runner -f ./config/values.yaml gitlab/gitlab-runner
+kubectl apply -f ./config/svaccounts.yaml
