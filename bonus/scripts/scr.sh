c565b4cabee9cf2fd898e5ecb85fe6a58b93e96f
zcolleen <semeneigin@yandex.ru>
2021-12-10 01:07:26 +0300

    gitlab start

--- bonus/scripts/scr.sh
+++ bonus/scripts/scr.sh
@@ -18,3 +18,16 @@ kubectl create namespace dev
 helm repo add gitlab https://charts.gitlab.io
 helm install --namespace gitlab gitlab-runner -f ./config/values.yaml gitlab/gitlab-runner
 kubectl apply -f ./config/svaccounts.yaml
+
+export GITLAB_HOME=/srv/gitlab
+docker run --detach \
+  --hostname gitlab.ndreadno.com \
+  --publish 443:443 --publish 80:80 --publish 2222:22 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  gitlab/gitlab-ee:latest
+
+echo "127.0.0.1           gitlab.ndreadno.com" >> /etc/hosts
