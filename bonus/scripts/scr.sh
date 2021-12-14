6835c809dfb72a5b54644e2f2b35149f9df12910
zcolleen <semeneigin@yandex.ru>
2021-12-14 23:12:09 +0300

    adding argcd to gitlab

--- bonus/scripts/scr.sh
+++ bonus/scripts/scr.sh
@@ -8,19 +8,31 @@ curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# argo cd
+curl -fsSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+
 apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg lsb-release
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 k3d cluster create gitlab
-export KUBECONFIG=$(sudo k3d kubeconfig write gitlab)
-chmod 644 $KUBECONFIG
-# kubectl create namespace gitlab
-# kubectl create namespace dev
-# helm repo add gitlab https://charts.gitlab.io
-# helm install --namespace gitlab gitlab-runner -f ./config/values.yaml gitlab/gitlab-runner
-# kubectl apply -f ./config/svaccounts.yaml
+
+cat <<- EOF > $HOME/.bashrc && source $HOME/.bashrc
+	export KUBECONFIG=$(sudo k3d kubeconfig write gitlab)
+	alias k=kubectl
+EOF
+chown $(id -u):$(id -g) $KUBECONFIG
+
+kubectl create ns gitlab
+kubectl apply -n gitlab -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n gitlab -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl apply -n gitlab -f ~/config/app.yaml
+
+echo Waiting for argocd web-interface endpoint...
+echo You CAN ssh into the machine in another terminal.
 
 
-cp -r /srv/gitlab ~/gitlab
+# cp -r /srv/gitlab ~/gitlab
+# gitlab
 chmod -R 0777 ~/gitlab
 export GITLAB_HOME=~/gitlab
 docker run --detach \
@@ -37,3 +49,10 @@ docker run --detach \
   gitlab/gitlab-ee:latest
 
 echo "127.0.0.1           gitlab.ndreadno.com" >> /etc/hosts
+
+params="-n argocd -l app.kubernetes.io/name=argocd-server --timeout=10m"
+kubectl wait --for=condition=available deployment $params
+kubectl wait --for=condition=ready pod $params
+
+pass=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath={.data.password} | base64 --decode)
+echo $pass | tee ~/argopasswd | sed "s|^|Argocd password: |g"
