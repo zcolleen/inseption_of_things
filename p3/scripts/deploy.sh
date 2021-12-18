2f6b8615c5254d61d5ca5b4c1b914d6715c230b6
Shimat <philip.yevseev@gmail.com>
2021-12-18 22:19:15 +0300

    port forward p3

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -8,19 +8,19 @@ cat <<- EOF > $HOME/.bashrc && source $HOME/.bashrc
 EOF
 sudo chown $(id -u):$(id -g) $KUBECONFIG
 
-k create ns argocd
-k apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-k patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-k apply -n argocd -f /vagrant/confs/app.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl apply -n argocd -f /vagrant/confs/app.yaml
 
 echo Waiting for argocd web-interface endpoint...
 echo You CAN ssh into the machine in another terminal.
 
 params="-n argocd -l app.kubernetes.io/name=argocd-server --timeout=10m"
-k wait --for=condition=available deployment $params
-k wait --for=condition=ready pod $params
+kubectl wait --for=condition=available deployment $params
+kubectl wait --for=condition=ready pod $params
 
 pass=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath={.data.password} | base64 --decode)
-echo $pass | tee /vagrant/argopasswd | sed "s|^|Argocd password: |g"
-k port-forward svc/argocd-server -n argocd 8080:80  --address=0.0.0.0 &
-k port-forward svc/argocd-server -n argocd 8443:443 --address=0.0.0.0 &
+echo $pass | tee /tmp/argopasswd | sed "s|^|Argocd password: |g"
+kubectl port-forward svc/argocd-server -n argocd 8080:80  --address=0.0.0.0 &
+kubectl port-forward svc/argocd-server -n argocd 8443:443 --address=0.0.0.0 &
